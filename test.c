8471d44c41e56f6a36676ebbf639b804323a34fb
우석 <mell03@naver.com>
2022-02-07 10:05:18 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,45 +1,57 @@
-// test.c
-#include <GLFW/glfw3.h>
-#include <cstdio>
+#include <GL/glx.h>
+#include <GL/gl.h>
+#include <unistd.h>
+#include <iostream>
 
-int main(void)
-{
-    GLFWwindow* window;
-    GLFWmonitor** monitors = NULL;
-    int monitorCount = 0;
+#define GLX_CONTEXT_MAJOR_VERSION_ARB		0x2091
+#define GLX_CONTEXT_MINOR_VERSION_ARB		0x2092
+typedef GLXContext (*GLXCREATECONTEXTATTRIBSARBPROC)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-    
-    monitors = glfwGetMonitors(&monitorCount);
-    printf("Num mon : %d", monitorCount);    
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        printf("could not create window");
-        glfwTerminate();
-        return -1;
-    }
+int main (int argc, char ** argv){
+	Display *dpy = XOpenDisplay(0);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	int nelements;
+	GLXFBConfig *fbc = glXChooseFBConfig(dpy, DefaultScreen(dpy), 0, &nelements);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	static int attributeList[] = { GLX_RGBA, GLX_DOUBLEBUFFER, GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE, 1, None };
+	XVisualInfo *vi = glXChooseVisual(dpy, DefaultScreen(dpy),attributeList);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	XSetWindowAttributes swa;
+	swa.colormap = XCreateColormap(dpy, RootWindow(dpy, vi->screen), vi->visual, AllocNone);
+	swa.border_pixel = 0;
+	swa.event_mask = StructureNotifyMask;
+	Window win = XCreateWindow(dpy, RootWindow(dpy, vi->screen), 0, 0, 100, 100, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask, &swa);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	XMapWindow (dpy, win);
 
-    glfwTerminate();
-    return 0;
-}
+	//oldstyle context:
+	//	GLXContext ctx = glXCreateContext(dpy, vi, 0, GL_TRUE);
+
+	std::cout << "glXCreateContextAttribsARB " << (void*) glXGetProcAddress((const GLubyte*)"glXCreateContextAttribsARB") << std::endl;
+	GLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB = (GLXCREATECONTEXTATTRIBSARBPROC) glXGetProcAddress((const GLubyte*)"glXCreateContextAttribsARB");
+
+	int attribs[] = {
+		GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+		GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+		0};
+
+	GLXContext ctx = glXCreateContextAttribsARB(dpy, *fbc, 0, true, attribs);
+
+	glXMakeCurrent (dpy, win, ctx);
+
+		glClearColor (0, 0.5, 1, 1);
+		glClear (GL_COLOR_BUFFER_BIT);
+		glXSwapBuffers (dpy, win);
+
+		sleep(1);
+
+		glClearColor (1, 0.5, 0, 1);
+		glClear (GL_COLOR_BUFFER_BIT);
+		glXSwapBuffers (dpy, win);
+
+		sleep(1);
+
+	ctx = glXGetCurrentContext(); 
+	glXDestroyContext(dpy, ctx); 
+	}
